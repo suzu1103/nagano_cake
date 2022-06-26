6f3b7c27f04517a610b2f6bd0f6c4174cad42bab
suzu1103 <4425krkh@gmail.com>
2022-06-26 10:40:44 +0000

    [Add]管理者　会員情報編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,26 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page])
+                        # .all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@item.id)
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,8 +4,16 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  # enum is_deleted: { invalid: true, valid: false }
+  # boolean型の時は不要↑
+  
   def full_name
     self.last_name + " " + self.first_name
   end
+  
+  def full_name_kana
+    self.last_name_kana + " " + self.first_name_kana
+  end
+
   
 end
