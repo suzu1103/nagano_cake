4e292f4e2c1bc7aae1f8b5cf970c07412e98e323
suzu1103 <4425krkh@gmail.com>
2022-06-27 17:00:14 +0000

    [Add]管理者　注文履歴

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  # enum is_active: { sale: true, stop_selling: false }
+  enum is_active: { sale: true, stop_selling: false }
   belongs_to :genre
-
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  def full_name
+    self.last_name + " " + self.first_name
+  end
+  
+  belongs_to :customer
+
+  
 end
