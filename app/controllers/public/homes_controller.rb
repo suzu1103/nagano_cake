897a166bc9f46c9b87997a2701b356bc5d97755d
suzu1103 <4425krkh@gmail.com>
2022-07-06 13:37:21 +0000

    [Add]商品詳細

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @items = Item.order('id DESC').limit(4)
+    # @item = Item.find(params[:id])
   end
 
   def about
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,13 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def after_sign_up_path_for(resource)
+    my_page_customers_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up)
+  end
+
 end
