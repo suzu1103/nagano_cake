9d560acf1c915dbb0b960c820f53c7ac5418ec42
suzu1103 <4425krkh@gmail.com>
2022-07-24 17:30:45 +0000

    [Add]注文履歴詳細「注文情報」まで

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -53,7 +53,7 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_details = @order.order_details
+    # @order_details = @order.order_details
   end
 
 
